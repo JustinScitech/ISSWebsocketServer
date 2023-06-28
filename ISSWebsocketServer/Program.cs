b6537def8efd47dbc38c7886f94897f78a2f4191
Justin Lau <jlau@bcsautomation.ca>
2023-06-28 12:32:49 -0400

    Added Azure SQL feature to update with websocket connection, added environment variables for Azure SQL account.

--- ISSWebsocketServer/Program.cs
+++ ISSWebsocketServer/Program.cs
@@ -2,18 +2,49 @@ using System.Collections.Concurrent;
 using System.Net.WebSockets;
 using System.Text;
 using Newtonsoft.Json.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System;
+using System.Data.SqlClient;
 
 namespace BCSServer
 {
     public class Program
     {
+        public List<StationInfo> stationData = new List<StationInfo>();
+        
+        public void OnGet()
+        {
+            try
+            {
+                DotNetEnv.Env.Load();
+                var password = Environment.GetEnvironmentVariable("your_password");
+                String connectionString = "Data Source=scmsbcs.database.windows.net,1433;Initial Catalog=rawdatadb;User ID=CloudSA1760a6ce;Password=" + password;
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM clients";
+                    using (SqlCommand command = new SqlCommand(sql,connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while(reader.Read())
+                            {
+                                StationInfo stationInfo = new StationInfo();
+                                stationInfo.num_of_people = reader.GetInt32(0);
+                                stationInfo.longitude = reader.GetDouble(1);
+                                stationInfo.latitude = reader.GetDouble(2);
+                                stationData.Add(stationInfo);
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e) { 
+                Console.WriteLine(e);
+                return;
+            }
+        }
+
         private static ConcurrentDictionary<string, WebSocket> _webSockets = new ConcurrentDictionary<string, WebSocket>();
         private static readonly HttpClient client = new HttpClient();
 
@@ -84,43 +115,93 @@ namespace BCSServer
         }
 
         private static async Task<string> FetchISSDataAsync()
+{
+    var response = await client.GetAsync("http://api.open-notify.org/iss-now.json");
+    var response2 = await client.GetAsync("http://api.open-notify.org/astros.json");
+    if (response.IsSuccessStatusCode && response2.IsSuccessStatusCode)
+    {
+        var json = await response.Content.ReadAsStringAsync();
+        var issData = JObject.Parse(json);
+
+        var json2 = await response2.Content.ReadAsStringAsync();
+        var humanData = JObject.Parse(json2);
+
+        // Parse JSON data
+        string latitude = issData["iss_position"]["latitude"].ToString();
+        string longitude = issData["iss_position"]["longitude"].ToString();
+        string num_of_people = humanData["number"].ToString();
+
+        // Get most recent entry from the station_table
+        StationInfo mostRecentEntry = GetMostRecentEntry();
+        // If the new data is different from the most recent entry in the table, then insert the new data
+        if (mostRecentEntry == null || mostRecentEntry.latitude != double.Parse(latitude) || mostRecentEntry.longitude != double.Parse(longitude) || mostRecentEntry.num_of_people != int.Parse(num_of_people))
         {
-            var response = await client.GetAsync("http://api.open-notify.org/iss-now.json");
-            var response2 = await client.GetAsync("http://api.open-notify.org/astros.json");
-            if (response.IsSuccessStatusCode && response2.IsSuccessStatusCode)
-            {
-                var json = await response.Content.ReadAsStringAsync();
-                var issData = JObject.Parse(json);
-
-                var json2 = await response2.Content.ReadAsStringAsync();
-                var humanData = JObject.Parse(json2);
-
-                var latitude = issData["iss_position"]["latitude"].ToString();
-                var longitude = issData["iss_position"]["longitude"].ToString();
-                var timestamp = issData["timestamp"].ToString();
-                long unixTimestamp = long.Parse(timestamp);
-                DateTime utcDate = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-                DateTime resultDate = utcDate.AddSeconds(unixTimestamp);
-                TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
-                resultDate = TimeZoneInfo.ConvertTimeFromUtc(resultDate, easternZone);
-                var numberPeople = humanData["number"].ToString();
-                var peopleInfo = "";
-
-                foreach (var person in humanData["people"]) { 
-                     peopleInfo += person["name"] + " | ";
-                }
-
-                var message = $"Timestamp: {resultDate}, Latitude: {latitude}, Longitude: {longitude} \nAnd there are {numberPeople} people currently in space. \nThe people include: {peopleInfo}";
-                return message;
-            }
-            else
-            {
-                return "Error: unable to fetch ISS data";
-            }
-
-            
-
+            InsertDataIntoStationTable(double.Parse(latitude), double.Parse(longitude), int.Parse(num_of_people));
         }
+
+        string peopleInfo = "";
+
+        foreach (var person in humanData["people"]) { 
+             peopleInfo += person["name"] + " | ";
+        }
+
+        string message = $"Latitude: {latitude}, Longitude: {longitude} \nAnd there are {num_of_people} people currently in space. \nThe people include: {peopleInfo}";
+        return message;
+    }
+    else
+    {
+        return "Error: unable to fetch ISS data";
+    }
+}
+
+private static StationInfo GetMostRecentEntry()
+{
+            DotNetEnv.Env.Load();
+            var password = Environment.GetEnvironmentVariable("your_password");
+            String connectionString = "Data Source=scmsbcs.database.windows.net,1433;Initial Catalog=rawdatadb;User ID=CloudSA1760a6ce;Password=" + password;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        String sql = "SELECT TOP 1 * FROM station_table ORDER BY id DESC"; // Assuming that there's an id column acting as a primary key
+        using (SqlCommand command = new SqlCommand(sql,connection))
+        {
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while(reader.Read())
+                {
+                    StationInfo stationInfo = new StationInfo();
+                    stationInfo.num_of_people = reader.GetInt32(0);
+                    stationInfo.longitude = reader.GetDouble(1);
+                    stationInfo.latitude = reader.GetDouble(2);
+                    return stationInfo;
+                }
+            }
+        }
+    }
+    return null;
+}
+
+private static void InsertDataIntoStationTable(double latitude, double longitude, int numberOfPeople)
+{
+    DotNetEnv.Env.Load();
+    var password = Environment.GetEnvironmentVariable("your_password");
+    String connectionString = "Data Source=scmsbcs.database.windows.net,1433;Initial Catalog=rawdatadb;User ID=CloudSA1760a6ce;Password=" + password;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        String sql = "INSERT INTO station_table (latitude, longitude, num_of_people) VALUES (@lat, @long, @num)"; // Column names in station_table
+        using (SqlCommand command = new SqlCommand(sql,connection))
+        {
+            command.Parameters.AddWithValue("@lat", latitude);
+            command.Parameters.AddWithValue("@long", longitude);
+            command.Parameters.AddWithValue("@num", numberOfPeople);
+            command.ExecuteNonQuery();
+        }
+    }
+}
+
 
         private static async Task BroadcastMessage(string senderId, string message)
         {
@@ -133,10 +214,18 @@ namespace BCSServer
 
                 {
                     await webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
-                    Console.WriteLine($"Sent message to {pair.Key}: {message}");
+                    Console.WriteLine($"Server sent message to {pair.Key}: {message}");
                 }
             }
         }
     }
+
+    public class StationInfo
+    {
+        public double longitude;
+        public double latitude;
+        public int num_of_people;
+    }
+
 }
 
